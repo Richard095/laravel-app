83a171a5d5a0d30470c86eb209125444c7b2d10e
Richard095 <ricardorm789@gmail.com>
2020-10-26 14:30:49 -0600

    C.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -73,8 +73,6 @@
                             </div>
                         {{-- @endif --}}
 
-
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
