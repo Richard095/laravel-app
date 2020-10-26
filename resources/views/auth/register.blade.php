4800d7759caea60e0b0ffb8ceb2de4df871dc389
Richard095 <ricardorm789@gmail.com>
2020-10-26 15:28:47 -0600

    Changes..

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -62,7 +62,7 @@
                         </div>
 
                         {{-- @if (Auth::User()) --}}
-                            {{-- <div class="form-group row">
+                            <div class="form-group row">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
                                 <div class="col-md-6">
                                     <select class="form-control" required name="rol" id="exampleFormControlSelect1">
@@ -70,7 +70,7 @@
                                         <option>Estudiante</option>
                                     </select>
                                 </div>
-                            </div> --}}
+                            </div>
                         {{-- @endif --}}
 
                         <div class="form-group row mb-0">
